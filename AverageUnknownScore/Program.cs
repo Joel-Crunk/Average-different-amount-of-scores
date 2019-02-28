24e852f4bc5acd0888e5d8d263be0726499ea123
Joel-Crunk <joel.crunk@outlook.com>
2019-02-28 16:21:03 -0500

    This commit will average different amounts of scores and return the grade

--- AverageUnknownScore/Program.cs
+++ AverageUnknownScore/Program.cs
@@ -4,12 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AverageUnknownScore
+namespace AverageScore
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter the amount of scores to be averaged: ");
+            int amount = int.Parse(Console.ReadLine());
+
+           
+            
+            int sum = 0;
+
+            for (int i = 0; i < amount; i++)
+            {
+              
+
+                Console.Write($"\n Enter score number: ");
+                int score = int.Parse(Console.ReadLine());
+               
+           
+
+                sum = sum + score;
+
+
+            }
+
+            double ave = sum / amount;
+            if (ave > 89)
+            {
+                Console.WriteLine($"\n\n{ave} is an \"A\" grade");
+            }
+            else if (ave > 79 && ave < 90)
+            {
+                Console.WriteLine($"\n\n{ave} is a \"B\" grade");
+            }
+            else if (ave > 69 && ave < 80)
+            {
+                Console.WriteLine($"\n\n{ave} is a \"C\" grade");
+            }
+            else if (ave > 59 && ave < 70)
+            {
+                Console.WriteLine($"\n\n{ave} is a \"D\" grade");
+            }
+            else
+            {
+                Console.WriteLine($"\n\n{ave} is a \"F\" grade");
+            }
+
+            Console.ReadLine();
+            //Console.WriteLine(ave);
+
         }
     }
 }
